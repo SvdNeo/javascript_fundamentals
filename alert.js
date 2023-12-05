37a38d4760fbb74e836c772db59a8e4d11b84f61
SvdNeo <sanjay.d@neoito.com>
2023-12-05 21:53:03 +0530

    A simple page

--- alert.js
+++ alert.js
@@ -8,8 +8,8 @@ const ourPlanetName = "Earth";
 alert(ourPlanetName);
 var currentUserName= "Sanjay";
 alert(currentUserName);
-const BIRTHDAY = "27.10.2022"; 
-const age = someCode(BIRTHDAY);
+// const BIRTHDAY = "27.10.2022"; 
+// const age = someCode(BIRTHDAY);
 
 let name1 = "Ilya";
 
@@ -18,3 +18,10 @@ alert( `hello ${1}` ); // hello 1
 alert( `hello ${"name"}` ); // hello name
 
 alert( `hello ${name1}` ); // hello Ilya
+
+let fullName = prompt("What is Your full name","")
+alert(`My  full name is ${fullName}`)
+
+let isLunch = confirm("Had Your lunch?");
+
+alert( isLunch ); 
