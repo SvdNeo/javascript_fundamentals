95ea9de92b6bc40da71201c95dc3f66bd7b68337
SvdNeo <sanjay.d@neoito.com>
2023-12-06 11:23:21 +0530

    Comparison

--- alert.js
+++ alert.js
@@ -29,36 +29,45 @@
 // 0, null, undefined, NaN, ""	 are all false
 
 // operators
-let a = 1, b = 1;
+// let a = 1, b = 1;
 
-let c = ++a; // 2
-console.log(c)
-let d = b++; // 1
-console.log(d)
+// let c = ++a; // 2
+// console.log(c)
+// let d = b++; // 1
+// console.log(d)
 
-let a1 = 2;
+// let a1 = 2;
 
-let x = 1 + (a1 *= 2);
-console.log(a1)// 4
-console.log(x) // 5
+// let x = 1 + (a1 *= 2);
+// console.log(a1)// 4
+// console.log(x) // 5
 
-// type conversions
-console.log("" + 1 + 0); // "10"
-console.log("" - 1 + 0); // -1
-console.log(true + false); // 1
-console.log(6 / "3"); // 2
-console.log("2" * "3"); // 6
-console.log(4 + 5 + "px"); //"9px"
-console.log("$" + 4 + 5); // "$45"
-console.log("4" - 2); //2
-console.log("4px" - 2); //NaN
-console.log("  -9  " + 5); // " -9  5"
-console.log("  -9  " - 5); // -14
-console.log(null + 1); // 1
-console.log(undefined + 1); // NaN
-console.log(" \t \n" - 2); //-2
+// // type conversions
+// console.log("" + 1 + 0); // "10"
+// console.log("" - 1 + 0); // -1
+// console.log(true + false); // 1
+// console.log(6 / "3"); // 2
+// console.log("2" * "3"); // 6
+// console.log(4 + 5 + "px"); //"9px"
+// console.log("$" + 4 + 5); // "$45"
+// console.log("4" - 2); //2
+// console.log("4px" - 2); //NaN
+// console.log("  -9  " + 5); // " -9  5"
+// console.log("  -9  " - 5); // -14
+// console.log(null + 1); // 1
+// console.log(undefined + 1); // NaN
+// console.log(" \t \n" - 2); //-2
 
-//Fix the addition
-let m = +prompt("First number?",1);
-let n = +prompt("Second number?",2);
-alert(m+n);
+// //Fix the addition
+// let m = +prompt("First number?",1);
+// let n = +prompt("Second number?",2);
+// alert(m+n);
+
+//comparisons
+console.log(5 > 4);//true
+console.log("apple" > "pineapple"); // false 
+console.log("2" > "12"); // true
+console.log(undefined == null); // true
+console.log(undefined === null); //false
+console.log(null == "\n0\n"); //false
+console.log(null === +"\n0\n")//false
