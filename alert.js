800bedebfc8b3868fdc2b8ac88b26d822a0284ae
SvdNeo <sanjay.d@neoito.com>
2023-12-06 10:05:06 +0530

    The postfix and prefix forms

--- alert.js
+++ alert.js
@@ -1,29 +1,37 @@
-alert("I'am Javascript");
-let name;
-let admin;
-name = "John";
-admin = name;
-alert(admin);
-const ourPlanetName = "Earth";
-alert(ourPlanetName);
-var currentUserName= "Sanjay";
-alert(currentUserName);
-// const BIRTHDAY = "27.10.2022"; 
-// const age = someCode(BIRTHDAY);
+// alert("I'am Javascript");
+// let name;
+// let admin;
+// name = "John";
+// admin = name;
+// alert(admin);
+// const ourPlanetName = "Earth";
+// alert(ourPlanetName);
+// var currentUserName= "Sanjay";
+// alert(currentUserName);
+// // const BIRTHDAY = "27.10.2022"; 
+// // const age = someCode(BIRTHDAY);
 
-let name1 = "Ilya";
+// let name1 = "Ilya";
 
-alert( `hello ${1}` ); // hello 1
+// alert( `hello ${1}` ); // hello 1
 
-alert( `hello ${"name"}` ); // hello name
+// alert( `hello ${"name"}` ); // hello name
 
-alert( `hello ${name1}` ); // hello Ilya
+// alert( `hello ${name1}` ); // hello Ilya
 
-let fullName = prompt("What is Your full name","")
-alert(`My  full name is ${fullName}`)
+// let fullName = prompt("What is Your full name","")
+// alert(`My  full name is ${fullName}`)
 
-let isLunch = confirm("Had Your lunch?");
+// let isLunch = confirm("Had Your lunch?");
 
-alert( isLunch ); 
+// alert( isLunch ); 
 
 // 0, null, undefined, NaN, ""	 are all false
+
+// operators
+let a = 1, b = 1;
+
+let c = ++a; // 2
+console.log(c)
+let d = b++; // 1
+console.log(d)
