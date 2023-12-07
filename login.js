a7cb869c41838ea949cb99a1471dc2b4cfb43c4a
SvdNeo <sanjay.d@neoito.com>
2023-12-07 11:23:22 +0530

    while loop Which values does the while loop show?

--- login.js
+++ login.js
@@ -1,19 +1,35 @@
-let login = prompt("Who's There ?");
-if(login === "Admin"){
-    let password = prompt("Enter the password");
-    if (password=== "The Master"){
-        alert("Welcome")
-    }
-    else if(password === "" || password === null){
-        alert("Cancelled")
-    }
-    else{
-        alert("Wrong Password")
-    }
+// let login = prompt("Who's There ?");
+// if(login === "Admin"){
+//     let password = prompt("Enter the password");
+//     if (password=== "The Master"){
+//         alert("Welcome")
+//     }
+//     else if(password === "" || password === null){
+//         alert("Cancelled")
+//     }
+//     else{
+//         alert("Wrong Password")
+//     }
     
-    }else if (login === "" || password === null){
-        alert("Cancelled")
-} 
-else{
-    alert("I don't Know you")
-}
+//     }else if (login === "" || password === null){
+//         alert("Cancelled")
+// } 
+// else{
+//     alert("I don't Know you")
+// }
+
+// // Loops 
+// let i = 3;
+
+// while (i) {
+//   alert( i-- );
+// } 
+// //last value is 1
+
+// let y = 0;
+// while (++y < 5) alert( y );
+// here the output would be 1,2,3,4
+
+let x = 0;
+while (x++ < 5) alert( x );
+//here the output is 1,2,3,4,5
