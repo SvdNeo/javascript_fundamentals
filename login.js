5c123b8730982d1265f249ceb69dd420ef960633
SvdNeo <sanjay.d@neoito.com>
2023-12-07 12:20:18 +0530

    for loop and while loop conversion

--- login.js
+++ login.js
@@ -30,6 +30,30 @@
 // while (++y < 5) alert( y );
 // here the output would be 1,2,3,4
 
-let x = 0;
-while (x++ < 5) alert( x );
+// let x = 0;
+// while (x++ < 5) alert( x );
 //here the output is 1,2,3,4,5
+
+// for (let i = 0; i < 5; i++) alert( i );
+// outputs 0,1,2,3,4
+
+// for (let i = 0; i < 5; ++i) alert( i );
+//outputs 0,1,2,3,4
+
+// for(let i =2; i<=10;i++){
+//     if(i%2 ==0){
+//         alert(i)
+//     }
+// }
+
+// for (let i = 0; i < 3; i++) {
+//     alert( `number ${i}!` );
+//   }
+
+  // rewrite this by using while loop
+  let c= 0;
+  while (c<3){
+    
+    alert( `number ${c}!` );
+    (c++);
+  }
